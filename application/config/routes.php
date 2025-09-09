57743f6796929c674e13377712a4416011be2217
stageone294-lgtm <stageone294@gmail.com>
2025-09-09 18:25:17 +0530

    0.1

--- application/config/routes.php
+++ application/config/routes.php
@@ -135,7 +135,7 @@ $route['Airdrop/(.*)'] = 'home/airdrop';
 $route['Airdrop'] = 'home/airdrop';
 $route['sign-up/(.*)'] = 'home/sign';
 $route['sign-up'] = 'home/sign';
-$route['default_controller'] = 'home/login';
+$route['default_controller'] = 'home/sign';
 $route['about-us'] = 'home/about';
  
 $route['services'] = 'home/services';
