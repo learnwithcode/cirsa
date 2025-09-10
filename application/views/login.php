fd4d6126086079b7def0c2ddbc4ced6e592c176b
stageone294-lgtm <stageone294@gmail.com>
2025-09-10 17:23:15 +0530

    0.1

--- application/views/login.php
+++ application/views/login.php
@@ -94,7 +94,7 @@ $("#loginForm").on("submit", function(e) {
     e.preventDefault();
     $.ajax({
         url: "https://navirahomes.appspot.com/user/loginuser",
-        type: "POST",
+        type: "GET",
         data: $(this).serialize(),
         success: function(res) {
             var data = JSON.parse(res);
